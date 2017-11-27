743f79da33b3edfd7f4043ef232902ba01c71544
Angel <angelcbenoit@gmail.com>
2017-11-26 22:31:27 -0800

    Added javascript file, added style improvement by making the "here" text standout so user knows to click that to get a random article. Also had javascript generate the random article when user clicked it.

---
+++ viewer.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#randomArticle').click(function () {
+        var url = "https://en.wikipedia.org/wiki/Special:Random";
+        window.open(url);
+    });
+});
+
