0f463fb9851253c6af6830ae69ed7b0140e761f6
Angel <angelcbenoit@gmail.com>
2017-11-28 22:26:07 -0800

    Made button fade when user clicked and made text input available. Also added wikipedia api feature so use can enter data to text input and all the urls to the page will appear to the user.

--- viewer.js
+++ viewer.js
@@ -5,3 +5,58 @@ $(document).ready(function () {
     });
 });
 
+
+// this link uses wikipedias api to find whatever the user is looking for
+//     https://en.wikipedia.org/w/api.php?action=query&list=allpages&apfrom=WHATEVERUSERWANTSTOSEE&format=json
+//
+// this link will give json formatted info and we can use the array to get the page id
+// we then use the page id to find the url using:
+//     https://en.wikipedia.org/?curid=PAGEID
+//         replacing PAGEID with the id we got from the json array, we not get the wiki url
+
+var pageId = "https://en.wikipedia.org/w/api.php?action=query&origin=*&list=allpages&apfrom=";
+var format = "&format=json";
+var url = "https://en.wikipedia.org/?curid=";
+
+$("#button").click(function () {
+    $(this).fadeOut();
+    $("#userInput").css("visibility", "visible");
+    $("#search").css("visibility", "visible");
+});
+
+$("#search").click(function (event) {
+    event.preventDefault();
+    getWikiId();
+    getURL();
+});
+
+function getWikiId() {
+    $("#back2").css("visibility", "visible");
+    var userInput = $("#userInput").val();
+    var link = pageId + userInput + format;
+    var arrayOfId = [];
+    $.getJSON(link, function (result) {
+        var pageArrayLength = result.query.allpages.length;
+        for (var i = 0; i < pageArrayLength; i++) {
+            arrayOfId[i] = result.query.allpages[i].pageid;
+        }
+        getURL(arrayOfId);
+    });
+}
+
+function getURL(list) {
+    var arrayOfURL = [];
+    for (var i = 0; i < list.length; i++) {
+        arrayOfURL[i] = url + list[i];
+    }
+    for (var x = 0; x < arrayOfURL.length; x++) {
+        $("#back2").append(arrayOfURL[x] + "<br>");
+    }
+}
+
+/* todo : get "title" from allpages array from json data and
+       put it in as display in #back2 for when user clicks on it,
+        another tab opens with the url*/
+/*
+    todo: fix the issue  of when user enters a data, it refreshes data when user enters new data 
+ */
